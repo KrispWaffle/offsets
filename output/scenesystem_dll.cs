316520d89b49984c837c6f97f60a9beca06425b7
Kyle Woods <ijwu345@gmail.com>
2026-04-28 22:20:53 -0700

    json

--- output/scenesystem_dll.cs
+++ output/scenesystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-29 05:15:23.792002900 UTC
+// 2026-04-29 05:19:31.366260800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: scenesystem.dll
