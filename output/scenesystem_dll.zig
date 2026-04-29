b4e3e3eae8892dde9e3b7dad0d2438b49b08da1f
Kyle Woods <ijwu345@gmail.com>
2026-04-28 19:26:18 -0700

    test

--- output/scenesystem_dll.zig
+++ output/scenesystem_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-29 01:28:22.293845900 UTC
+// 2026-04-29 02:25:37.154976100 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {
--- output/soundsystem_dll.cs
+++ output/soundsystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-29 01:28:22.293845900 UTC
+// 2026-04-29 02:25:37.154976100 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: soundsystem.dll
