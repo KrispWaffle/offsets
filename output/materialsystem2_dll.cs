223f2fed73cf4deb697764b2f5c868d9c8f78fdf
Kyle Woods <ijwu345@gmail.com>
2026-05-07 16:05:08 -0700

    game update

--- output/materialsystem2_dll.cs
+++ output/materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-07 00:31:00.916915200 UTC
+// 2026-05-07 22:41:12.445359 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
