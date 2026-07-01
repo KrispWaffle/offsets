1dc1da880b33d7dc07b7db4956c5d6c9d63da299
Kyle Woods <ijwu345@gmail.com>
2026-07-01 12:32:52 -0700

    gameupdate

--- output/engine2_dll.cs
+++ output/engine2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-15 17:37:17.374857200 UTC
+// 2026-07-01 19:31:29.770511 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: engine2.dll
